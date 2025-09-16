8393810a3d229a5f8ef44c10813fa555a096813f
vikramkumarsfe <dk677057@gmail.com>
2025-09-16 16:56:46 +0530

    Connected Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 import cors from "cors";
 import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+
+const supabase = createClient(process.env.DATABASE_URL,process.env.DATABASE_KEY);
+
+async function testConnection() {
+  try {
+    const { error } = await supabase.from("Posts").select("*").limit(1);
+
+    if (error) {
+      console.error("Supabase connection failed:", error.message);
+    } else {
+      console.log("Supabase connected!");
+    }
+  } catch (err) {
+    console.error("Unexpected error while connecting:", err.message);
+  }
+}
+
+testConnection();
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
