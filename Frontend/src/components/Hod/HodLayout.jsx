7ff516f548278b77436af198d28bffcd3637a6a0
Shivam Kumar <sk744826744@gmail.com>
2025-09-28 10:57:17 +0530

    fixed header

--- Frontend/src/components/Hod/HodLayout.jsx
+++ Frontend/src/components/Hod/HodLayout.jsx
@@ -1,7 +1,6 @@
 // src/components/AppLayout.jsx
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
-import Header from "./Header";
 import VerifyResults from "./Pages/VerifyResult"; // Example Content
 import StudentDropout from "./Pages/StudentDropout";
 import ApproveStudents from "./Pages/ApproveStudents";
@@ -41,7 +40,12 @@ const HodLayout = () => {
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col">
         {/* Header Component */}
-        <Header />
+        <header className="bg-white border-b h-16 flex items-center justify-end px-6">
+          <span className="text-gray-700 mr-4">Welcome, Dr. [Name]</span>
+          <div className="w-10 h-10 rounded-full bg-gray-300">
+            {/* Placeholder for User Profile Image */}
+          </div>
+        </header>
 
         {/* Page Content */}
         <main className="p-6 md:p-8 flex-1 overflow-auto">
