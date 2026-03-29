798ac38e4adcb9f3a1fa868a21c74656a45549ea
khushi29105 <156419739+khushi29105@users.noreply.github.com>
2026-03-29 15:13:30 +0530

    Add project files

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,7 @@
+export const register = async (req, res) => {
+  res.status(200).json({ message: "Register endpoint" })
+}
+
+export const login = async (req, res) => {
+  res.status(200).json({ message: "Login endpoint" })
+}
